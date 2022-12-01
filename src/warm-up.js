b779c90e4519ed02babc357189d32edf17fde4e9
Joshua Martin <ursa8minority@gmail.com>
2022-11-30 17:25:18 -0700

    Add Variables answers

--- src/warm-up.js
+++ src/warm-up.js
@@ -10,8 +10,8 @@
 // Initialize two new variables, "title", and "pages", and assign values to them with
 // a book's title and page count.
 
-// var title = "Harry Potter";
-// var ...
+ var title = "The Gunslinger";
+ var pages = 700
 
 
 
@@ -19,20 +19,25 @@
 // condensed the book, use a built in math operator to make the page count a quarter
 // of what it currently is 
 
+pages = 500
+
 
 
 // Initialize a new variable "movie", but leave it undefined
 
+var movie = ""
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
 
+var sequel = `${title}:Returns`
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
+var isFavorite = true
 
 
 
@@ -41,6 +46,7 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+var isShortStory = (pages < 25 === true)
 
 
 //------------------------- Conditionals -------------------------//
