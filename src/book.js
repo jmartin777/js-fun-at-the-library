b40056622d163d60d3147d4fa1448d025b6c394b
Joshua Martin <ursa8minority@gmail.com>
2022-12-02 18:54:01 -0700

    Add final answers books.js

--- src/book.js
+++ src/book.js
@@ -12,15 +12,37 @@ return character
 }
 
 function saveReview(review, array) {
+  if (array.includes(review) === false)
   array.push(review)
 }
+
+function calculatePageCount(bookTitle) {
+  var bookPageCount = bookTitle.length * 20
+  return bookPageCount;
+
+}
+function writeBook(createTitle, bookCharacter, genre) {
+  var book = {
+    title: createTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(createTitle),
+    genre: genre
+  }
+
+  return book
+}
+
+
+    
+
+
 
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
+  calculatePageCount,
+  writeBook,
   // editBook
 }
