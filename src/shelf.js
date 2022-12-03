dfb67ff3d6c63c96d7cb7d3e79977d9f3a2b106e
Joshua Martin <ursa8minority@gmail.com>
2022-12-03 16:09:04 -0700

    Add final answers Shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,38 @@
+function shelfBook(books,shelf){ 
+  if (shelf.length < 3){
+ shelf.unshift(books)
+}
+}
+
+function unshelfBook (book,shelf) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book)
+    return shelf.splice(i, 1)
+  }
+  
+
+}
+
+function listTitles(shelf) { 
+var titles = [] 
+for (i = 0; i < shelf.length; i++) {
+  titles.push(shelf[i].title) 
+} return titles.join(', ')
+}
+
+function searchShelf (shelf, book) {
+  for (var i = 0; i < shelf.length; i++) { 
+    if (shelf[i].title === book) {
+      return true
+    }
+  } return false
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+    shelfBook,
+    unshelfBook,
+    listTitles,
+    searchShelf
 };
