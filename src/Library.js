3205609cffd950b31f7974e6081e4153ee524d9d
Joshua Martin <ursa8minority@gmail.com>
2022-12-03 21:35:12 -0700

    Add final answers Library.js

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,30 @@
+function createLibrary(library) {
+  return {
+    name: library,
+    shelves: {
+      fantasy: [], fiction: [], nonFiction: []
+    } 
+  }
+}
+
+function addBook(library, book) {
+library.shelves[book.genre].push(book)
+}
+
+function checkoutBook(library, book, genre) {
+  var findBook = library["shelves"][genre]
+  for (i = 0; i < findBook.length; i++) {
+    if (book === findBook[i].title)
+  findBook.splice(i, 1)
+  return `You have now checked out ${book} from the ${library.name}`
+  }
+  return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+}
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+   createLibrary,
+   addBook,
+   checkoutBook
 };
